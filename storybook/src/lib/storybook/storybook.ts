ea66efa26a4de5e4aeaad4e664eb5018f6a7cef4
voyage200 <hello_cmd@outlook.com>
2026-05-22 01:36:16 +0800

    good storybook config!

---
+++ storybook/src/lib/storybook/storybook.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-storybook',
+  imports: [],
+  templateUrl: './storybook.html',
+  styleUrl: './storybook.css',
+})
+export class Storybook {}
